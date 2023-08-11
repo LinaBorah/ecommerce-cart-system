ea205829b38ff6b24f4c202241f5f283504043c1
Lina Borah <26639248+LinaBorah@users.noreply.github.com>
2023-08-11 06:40:10 +0530

    Initial parts creation of the ecommerce websites

---
+++ src/Pages/Shop/Shop.jsx
@@ -0,0 +1,14 @@
+import { Heading, VStack } from '@chakra-ui/react'
+export default function Shop() {
+    return (
+        <>
+            <Heading>
+                Lina's Store
+            </Heading>
+            <VStack>
+                <Products/>
+            </VStack>
+        </>
+
+    )
+}
