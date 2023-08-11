14f2495e5d96853a1ab7f497f15c76ff25a96501
Lina Borah <26639248+LinaBorah@users.noreply.github.com>
2023-08-11 07:16:58 +0530

    Initial folder changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 //import logo from './logo.svg';
-import { ChakraProvider } from '@chakra-ui/react';
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Shop from './Pages/Shop/Shop';
+
 function App() {
   return (
-    <div className='App'>
-      <ChakraProvider>
-        <Router>
-          <Navbar/>
-          <Routes>
-            <Route path='/' element={<Shop/>}/>
-            <Route path='/cart'></Route>
-          </Routes>
-        </Router>
-      </ChakraProvider>
+    <div className="App">
+     
     </div>
-
-
   );
 }
 
