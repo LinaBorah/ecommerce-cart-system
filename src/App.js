bb3222110f58051f1ec89ef34bc665cf3d1ea42d
Lina Borah <26639248+LinaBorah@users.noreply.github.com>
2023-08-11 17:29:35 +0530

    Added the Navigation Bar with two links for two pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 //import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './Components/Navbar';
+import { ChakraProvider } from '@chakra-ui/react';
 function App() {
   return (
     <div className="App">
-     
+      <ChakraProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/' />
+            <Route path='/cart' />
+          </Routes>
+        </Router>
+      </ChakraProvider>
     </div>
   );
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {ShoppingCart} from 'phosphor-react';
+import {HStack, VStack} from '@chakra-ui/react'
+export default function Navbar(){
+    return(
+        <div className='navbar'>
+            <div className='links'>
+                <HStack
+                spacing={4}
+                backgroundColor={'#1B023E'}
+                color={'white'}
+                height={'45px'}
+                alignItems={'center'}
+                justifyContent={'flex-end'}
+                padding={'30px'}
+                fontWeight={'medium'}
+                fontSize={'lg'}
+                fontFamily={'heading'}>
+                <Link to='/'>Shop</Link>
+                <Link to='/cart'>
+                    <ShoppingCart size={32}/>
+                </Link>
+                </HStack>
+            </div>
+        </div>
+    )
+}
